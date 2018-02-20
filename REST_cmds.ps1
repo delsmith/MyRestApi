6329423171db9d296d433bc37b675b821c103274
Dels <del.b.smith@gmail.com>
2018-02-21 09:35:05 +1100

    Update test script
    Generalise IP address and port

--- REST_cmds.ps1
+++ REST_cmds.ps1
@@ -1,6 +1,17 @@
-﻿$url = "http://localhost:64469/api"
-Invoke-RestMethod -uri "$url/Values" -method GET -UseDefaultCredentials
-Invoke-RestMethod -uri "$url/Values" -method PUT -Body "I PUT this." -UseDefaultCredentials
-Invoke-RestMethod -uri "$url/Values" -method POST -Body "I POSTed this." -UseDefaultCredentials
-Invoke-RestMethod -uri "$url/Values" -method GET -UseDefaultCredentials
-Invoke-RestMethod -uri "$url/Values/3" -method GET -UseDefaultCredentials
+﻿$DnpSimHost = "10.211.55.4"
+$Port = ":21212"
+$url = "http://$DnpSimHost$Port/api"
+"Invoke-RestMethod -uri ""$url/DnpSim"" -method GET -UseDefaultCredentials"
+Invoke-RestMethod -uri "$url/DnpSim" -method GET -UseDefaultCredentials
+
+"`r`nInvoke-RestMethod -uri ""$url/DnpSim"" -method PUT -Body ""I PUT this."" -UseDefaultCredentials"
+Invoke-RestMethod -uri "$url/DnpSim" -method PUT -Body "I PUT this." -UseDefaultCredentials
+
+"`r`nInvoke-RestMethod -uri ""$url/DnpSim"" -method POST -Body ""I POSTed this."" -UseDefaultCredentials"
+Invoke-RestMethod -uri "$url/DnpSim" -method POST -Body "I POSTed this." -UseDefaultCredentials
+
+"`r`nInvoke-RestMethod -uri ""$url/DnpSim"" -method GET -UseDefaultCredentials"
+Invoke-RestMethod -uri "$url/DnpSim" -method GET -UseDefaultCredentials
+
+"`r`nInvoke-RestMethod -uri ""$url/DnpSim/3"" -method GET -UseDefaultCredentials"
+Invoke-RestMethod -uri "$url/DnpSim/3" -method GET -UseDefaultCredentials
